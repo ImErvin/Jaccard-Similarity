a2c29dab920ae9054613683c96e318404be50c41
Ervin Mamutov <erviin@live.ie>
2018-01-07 23:50:28 +0000

    Added a document object

---
+++ src/ie/gmit/sw/Document.java
@@ -0,0 +1,31 @@
+package ie.gmit.sw;
+
+import java.util.*;
+
+public class Document {
+	private String title;
+	private Set<Integer> hashes = new TreeSet<Integer>();
+	
+	public Document(String title, Set<Integer> hashes) {
+		super();
+		this.title = title;
+		this.hashes = hashes;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Set<Integer> getHashes() {
+		return hashes;
+	}
+
+	public void setHashes(Set<Integer> hashes) {
+		this.hashes = hashes;
+	}
+	
+}
