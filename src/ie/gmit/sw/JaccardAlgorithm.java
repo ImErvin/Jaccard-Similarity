3548c6916985f87c9983517bd6155763f93ab8e1
Ervin Mamutov <erviin@live.ie>
2018-01-08 16:52:27 +0000

    Added algorthim Jaccard - not yet tested

--- src/ie/gmit/sw/JaccardAlgorithm.java
+++ src/ie/gmit/sw/JaccardAlgorithm.java
@@ -1,13 +1,19 @@
 package ie.gmit.sw;
 
 import java.util.Set;
+import java.util.TreeSet;
 
-public class JaccardAlgorithm implements SimilarityAlgo{
-
+public class JaccardAlgorithm implements SimilarityAlgorithm{
+	
+	private Set<Integer> n = new TreeSet<Integer>();
+	private int intersectionCardinality;
+	
 	@Override
 	public double compareSimilarity(Set<Integer> a, Set<Integer> b) {
-		// TODO Auto-generated method stub
-		return 0;
+		n = a;
+		n.retainAll(b);
+		intersectionCardinality = n.size();
+		return intersectionCardinality / (a.size() + b.size()) - intersectionCardinality;
 	}
 
 }
--- src/ie/gmit/sw/SimilarityAlgorithm.java
+++ src/ie/gmit/sw/SimilarityAlgorithm.java
@@ -2,6 +2,6 @@ package ie.gmit.sw;
 
 import java.util.Set;
 
-public interface SimilarityAlgo {
+public interface SimilarityAlgorithm {
 	public double compareSimilarity(Set<Integer> a, Set<Integer> b);
 }
